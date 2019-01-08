5e87862c0c68f0e498d3c56a98144fc434af64e7
Evgeny Khaliper <eugen.none@gmail.com>
2019-01-08 15:58:24 +0200

    123213

--- s3store/main.tf
+++ s3store/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket_object" "object" {
   bucket  = "${var.bucket}"
-  key     = "${var.sandbox_id}.json"
+  key     = "${var.sandboxid}.json"
   content = "[\"${var.item1}\", \"${var.item2}\", \"${var.item3}\"]"
 }
