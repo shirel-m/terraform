e5517c346450c9de268712d237c7e424a5654e3f
Evgeny Khaliper <eugen.none@gmail.com>
2019-01-08 13:27:10 +0200

    just fix

--- s3store/main.tf
+++ s3store/main.tf
@@ -1,10 +1,5 @@
-resource "aws_s3_bucket" "b" {
-  bucket = "${var.bucket}"
-  acl    = "private"
-}
-
 resource "aws_s3_bucket_object" "object" {
-  bucket  = "${aws_s3_bucket.b.bucket}"
-  key     = "data.json"
+  bucket  = "${var.bucket}"
+  key     = "${var.prefix}${var.sandbox_id}.json"
   content = "['Jenkins', 'Linux', 'Docker', 'Gamal', 'Python']"
 }
