39589a5afc60e2e8c6428c25ec1a12be5d86a268
Evgeny Khaliper <eugen.none@gmail.com>
2019-03-06 10:36:24 +0200

    Update main.tf

--- s3store/main.tf
+++ s3store/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket_object" "object" {
   bucket  = "${var.bucket}"
-  key     = "${var.sandbox_id}.json"
+  key     = "${var.SANDBOX_ID}.json"
   content = "[\"${var.item1}\", \"${var.item2}\", \"${var.item3}\"]"
 }
