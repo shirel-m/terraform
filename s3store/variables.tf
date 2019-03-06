3091ecc15b306298295c4a138e7ac0977ca5fc0f
Evgeny Khaliper <eugen.none@gmail.com>
2019-03-06 14:55:37 +0200

    Update variables.tf

--- s3store/variables.tf
+++ s3store/variables.tf
@@ -3,3 +3,6 @@ variable "SANDBOX_ID" {}
 variable "item1" {}
 variable "item2" {}
 variable "item3" {}
+variable "AWS_ACCESS_KEY_ID" {}
+variable "AWS_SECRET_ACCESS_KEY" {}
+variable "AWS_DEFAULT_REGION" {}
