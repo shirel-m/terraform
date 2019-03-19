814abfaa6bf3f29c270ceb94f2245874e6a171e4
Evgeny Khaliper <eugen.none@gmail.com>
2019-03-19 10:14:35 +0200

    aws provider

--- s3store/main.tf
+++ s3store/main.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+  access_key = "${var.AWS_ACCESS_KEY_ID}"
+  secret_key = "${var.AWS_SECRET_ACCESS_KEY}"
+  region     = "${var.AWS_DEFAULT_REGION}"
+}
 resource "aws_s3_bucket_object" "object" {
   bucket  = "${var.bucket}"
   key     = "${var.SANDBOX_ID}.json"
