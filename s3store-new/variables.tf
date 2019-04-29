58354ed7430ca26d164c3696a7bef38c4f8fbe8b
asafc64 <asafc64@gmail.com>
2019-04-29 14:48:57 +0300

    Create variables.tf

---
+++ s3store-new/variables.tf
@@ -0,0 +1,11 @@
+
+  
+variable "bucket" {}
+variable "SANDBOX_ID" {}
+variable "SANDBOX_DNS" {}
+variable "item1" {}
+variable "item2" {}
+variable "item3" {}
+variable "AWS_ACCESS_KEY_ID" {}
+variable "AWS_SECRET_ACCESS_KEY" {}
+variable "AWS_DEFAULT_REGION" {}
