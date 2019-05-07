b4736288cc5cbf63c4ab6db08c12696157264ab5
shaykeren <shay.keren@gmail.com>
2019-05-07 11:15:41 +0300

    add rds

---
+++ rds/main.tf
@@ -0,0 +1,13 @@
+provider "aws" { }
+
+resource "aws_db_instance" "default" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "${var.DB_NAME}-${var.SANDBOX_ID}"
+  username             = "${var.DB_USER}"
+  password             = "${var.DB_PASS}"
+  parameter_group_name = "default.mysql5.7"
+}
---
+++ rds/variables.tf
@@ -0,0 +1,4 @@
+variable "DB_NAME" {}
+variable "DB_USER" {}
+variable "DB_PASS" {}
+variable "SANDBOX_ID" {}
