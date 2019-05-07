75d7f829e425f7bcdd70bcc7cf4177fbded25273
shaykeren <shay.keren@gmail.com>
2019-05-07 15:53:36 +0300

    aws_region

--- s3-shay/main.tf
+++ s3-shay/main.tf
@@ -1,6 +1,9 @@
 provider "aws" {}
+data "aws_region" "current" {
+  provider = "aws.region"
+}
 resource "aws_s3_bucket_object" "object" {
   bucket  = "${var.bucket}"
   key     = "${var.SANDBOX_ID}.json"
-  content = "[\"${var.SANDBOX_DNS}\", \"${var.item1}\", \"${var.item2}\", \"${var.item3}\", \"${data.aws_region}\"]"
+  content = "[\"${var.SANDBOX_DNS}\", \"${var.item1}\", \"${var.item2}\", \"${var.item3}\", \"${data.aws_region.current.name}\"]"
 }
