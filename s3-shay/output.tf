efdfaf1cea4e704b415e1948a564457ef9f53302
shaykeren <shay.keren@gmail.com>
2019-06-03 12:03:27 +0300

    add outputs

--- s3-shay/output.tf
+++ s3-shay/output.tf
@@ -1,3 +1,42 @@
-output "other_variable_value" {
-  value = "${var.Other}"
+output "output_from_content_variable" {
+  value = "${var.CONTENT}"
 }
+
+output "output_str" {
+  value = "text"
+}
+
+output "output_num" {
+  value = 100
+}
+
+output "output_list_numbers" {
+  value = [1, 2, 3]
+}
+output "output_list_str" {
+  value = ["a", "b", "c"]
+}
+
+output "output_map" {
+  value = {
+      "user-a": "a-topic",
+      "user-b": "c-topic",
+      "user-c": "c-topic"
+  }
+}
+
+output "output_obj" {
+  value = {
+    "age": 52,
+    "name": "John",
+    "user": [
+      {
+        "user-a": "a-topic",
+        "user-b": "c-topic",
+        "user-c": "c-topic"
+      }
+    ]
+  }
+}
+
+
--- s3-shay/variables.tf
+++ s3-shay/variables.tf
@@ -1,3 +1,12 @@
 variable "BUCKET_NAME" {}
 variable "SANDBOX_ID" {}
 variable "CONTENT" {}
+
+
+variable "testmapvar" {
+  type = "map"
+  default = {
+    key1 = "value1"
+    key2 = "value2"
+  }
+}
