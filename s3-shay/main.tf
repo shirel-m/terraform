700c6b424476a4f3a1ee03376ebd0b5e49914609
shaykeren <shay.keren@gmail.com>
2019-07-01 08:26:24 +0300

    fix

--- s3-shay/main.tf
+++ s3-shay/main.tf
@@ -1,5 +1,7 @@
-provider "aws" {}
-data "aws_region" "current" {}
+provider "aws" {
+  region = "eu-west-1"
+}
+
 resource "aws_s3_bucket_object" "object" {
   bucket  = "${var.BUCKET_NAME}"
   key     = "${var.SANDBOX_ID}.json"
