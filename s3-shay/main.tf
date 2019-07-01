2b855708332003d11221dbd18202e8687e4deada
shaykeren <shay.keren@gmail.com>
2019-07-01 09:16:44 +0300

    ch

--- s3-shay/main.tf
+++ s3-shay/main.tf
@@ -5,5 +5,5 @@ provider "aws" {
 resource "aws_s3_bucket_object" "object" {
   bucket  = "${var.BUCKET_NAME}"
   key     = "${var.SANDBOX_ID}.json"
-  content = "{ \"Content\": \"${var.CONTENT}\", \"FROM_PUBLISHER_1\": \"${var.FROM_PUBLISHER_1}\", \"FROM_PUBLISHER_2\": \"${var.FROM_PUBLISHER_2}\", \"Default Region\": \"${data.aws_region.current.name}\" }"
+  content = "{ \"Content\": \"${var.CONTENT}\", \"FROM_PUBLISHER_1\": \"${var.FROM_PUBLISHER_1}\", \"FROM_PUBLISHER_2\": \"${var.FROM_PUBLISHER_2}\" }"
 }
