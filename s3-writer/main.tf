929f5117202f288b7ee5be3a17a67d83892aed26
shaykeren <shay.keren@gmail.com>
2019-07-17 16:01:07 +0300

    s3_file_url

--- s3-writer/main.tf
+++ s3-writer/main.tf
@@ -5,5 +5,14 @@ provider "aws" {
 resource "aws_s3_bucket_object" "object" {
   bucket  = "${var.BUCKET_NAME}"
   key     = "${var.SANDBOX_ID}"
-  content = "UserContent:${var.USER_CONTENT}\nSERVER_NAME:${var.SERVER_NAME}\nSERVER_VERSION:${var.SERVER_VERSION}\nOUTPUT_PUBLISHER_STR:${var.OUTPUT_PUBLISHER_STR}\nOUTPUT_PUBLISHER_OBJ:${var.OUTPUT_PUBLISHER_OBJ}\nOUTPUT_PUBLISHER_MAP:${var.OUTPUT_PUBLISHER_MAP}\nOUTPUT_PUBLISHER_LIST:${var.OUTPUT_PUBLISHER_LIST}"
+  content = "USER_CONTENT:${var.USER_CONTENT}\nSERVER_NAME:${var.SERVER_NAME}\nSERVER_VERSION:${var.SERVER_VERSION}\nOUTPUT_PUBLISHER_STR:${var.OUTPUT_PUBLISHER_STR}\nOUTPUT_PUBLISHER_OBJ:${var.OUTPUT_PUBLISHER_OBJ}\nOUTPUT_PUBLISHER_MAP:${var.OUTPUT_PUBLISHER_MAP}\nOUTPUT_PUBLISHER_LIST:${var.OUTPUT_PUBLISHER_LIST}"
+}
+
+data "external" "presign" {
+  program = ["bash", "presign.sh", "${var.BUCKET_NAME}", "${var.SANDBOX_ID}"]
+  depends_on = [aws_s3_bucket_object.object]
+}
+
+output "s3_file_url" {
+  value = "${data.external.presign.result.url}"
 }
---
+++ s3-writer/presign.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "{ \"url\": \"$(aws s3 presign s3://$1/$2 --expires-in 120)\" }"
