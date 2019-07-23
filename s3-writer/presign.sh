222ce6d5963a43a68327c99be76ad2752a992a8c
shaykeren <shay.keren@gmail.com>
2019-07-23 08:52:08 +0300

    f

--- s3-writer/presign.sh
+++ s3-writer/presign.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 export AWS_DEFAULT_REGION='eu-west-1'
-if [ "$3" != 'none']; then
-    access_key="$3"
-    secret_key="$4"
+if [ "$3" != 'none' ]; then
     export AWS_ACCESS_KEY_ID="$3"
     export AWS_SECRET_ACCESS_KEY="$4"
     echo "{ \"url\": \"$(aws s3 presign s3://$1/$2 --expires-in 1800)\" }"
