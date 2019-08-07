073ab7e23931b225196c634a17efdaea6ba985ca
AlexanderO <alex.o@quali.com>
2019-08-07 16:03:46 +0300

    username + password as outputs

--- aws-rds-alex/outputs.tf
+++ aws-rds-alex/outputs.tf
@@ -1,3 +1,11 @@
 output "rds_endpoint" {
   value = "${aws_db_instance.default.endpoint}"
 }
+
+output "rds_username" {
+  value = "${aws_db_instance.default.username}"
+}
+
+output "rds_password" {
+  value = "${aws_db_instance.default.password}"
+}
