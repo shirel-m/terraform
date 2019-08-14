7b43317e20a3e9b50fe744e945fb1ccd5f506f8e
Shay Keren <shay.keren@gmail.com>
2019-08-14 14:09:43 +0300

    Update variables.tf

--- s3-writer/variables.tf
+++ s3-writer/variables.tf
@@ -4,9 +4,5 @@ variable "USER_CONTENT" {}
 variable "SERVER_NAME" {}
 variable "SERVER_VERSION" {}
 variable "OUTPUT_PUBLISHER_STR" {}
-variable "OUTPUT_PUBLISHER_OBJ" {}
-variable "OUTPUT_PUBLISHER_MAP" {}
-variable "OUTPUT_PUBLISHER_LIST" {}
-
 variable "AWS_ACCESS_KEY" {}
 variable "AWS_SECRET_ACCESS" {}
