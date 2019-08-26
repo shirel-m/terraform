513c6f94b604e7b153ceb2a426c9fb2efa5c4f5d
shaykeren <shay.keren@gmail.com>
2019-08-26 09:23:34 +0300

    changes

--- rds-mysql/variables.tf
+++ rds-mysql/variables.tf
@@ -1,25 +1,20 @@
 variable "db_name" {
-  default     = "demo_db"
   description = "db name"
 }
 
 variable "username" {
-  default     = "admin"
   description = "User name"
 }
 
 variable "password" {
-  default = "Quali0525112708"
   description = "Database password"
 }
 
 variable "sandbox_id" {
-  default = "cqfhxy29fb02c1"
   description = "Colony sandbox id"
 }
 
 variable "vpc_id" {
-  default = "vpc-063377c4f43e7895e"
   description = "Colony sandbox vpc"
 }
 
