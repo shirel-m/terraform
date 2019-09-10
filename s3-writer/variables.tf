30914a500ed6adc1079d7b0690d3453720e2110b
shaykeren <shay.keren@gmail.com>
2019-09-10 16:21:48 +0300

    update terraform

--- s3-writer/variables.tf
+++ s3-writer/variables.tf
@@ -1,8 +1,14 @@
 variable "BUCKET_NAME" {}
-variable "SANDBOX_ID" {}
 variable "USER_CONTENT" {}
 variable "SERVER_NAME" {}
 variable "SERVER_VERSION" {}
 variable "OUTPUT_PUBLISHER_STR" {}
 variable "AWS_ACCESS_KEY" {}
 variable "AWS_SECRET_ACCESS" {}
+variable "VIRTUAL_NETWORK_ID" {}
+variable "PUBLIC_ADDRESS" {}
+variable "PUBLIC_ADDRESS_OLD" {}
+variable "SANDBOX_ID" {}
+variable "SANDBOX_ID_OLD" {}
+variable "OUTPUTS" {}
+variable "ALL_IN_ONE" {}
