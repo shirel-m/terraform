1dfc037014e38b868d2c721268bff22b198eed82
svetaquali <svetlana.c@quali.com>
2019-11-12 14:24:48 +0200

    space

--- azure-sql-db/variables.tf
+++ azure-sql-db/variables.tf
@@ -17,18 +17,18 @@ variable "sandbox_id" {
   description = "Colony sandbox id"
 }
 
-variable "client_id"{
+variable "client_id" {
   description = "The Client ID which should be used"
 }
 
-variable "client_secret"{
+variable "client_secret" {
   description = "The Client Secret which should be used"
 }
 
-variable "subscription_id"{
+variable "subscription_id" {
   description = "The Subscription ID which should be used"
 }
 
-variable "tenant_id"{
+variable "tenant_id" {
   description = "The Tenant ID which should be used"
 }
