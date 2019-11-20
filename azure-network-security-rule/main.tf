55ec7ef70c37f3a5f20073f4a391a3acc4970b25
AlexanderO <alex.o@quali.com>
2019-11-20 09:31:47 +0200

    service provider

--- azure-network-security-rule/main.tf
+++ azure-network-security-rule/main.tf
@@ -1,3 +1,11 @@
+provider "azurerm" {
+  version = "=1.28.16"
+  client_id = "${var.client_id}"
+  client_secret = "${var.client_secret}"
+  subscription_id = "${var.subscription_id}"
+  tenant_id = "${var.tenant_id}"
+}
+
 resource "azurerm_resource_group" "resource_group" {
   name     = "${var.resource_group}"
   location = "${var.location}"
