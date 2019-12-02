e7c0f0fe88d4a56c429c1d3b8d093f4cfe91bd5a
svetaquali <svetlana.c@quali.com>
2019-12-02 18:07:54 +0200

    create rg

--- sveta-test/main.tf
+++ sveta-test/main.tf
@@ -13,8 +13,13 @@ variable "rg_name" {
   description = "resource group name"
 }
 
-data "azurerm_resource_group" "test" {
-  name = "${var.rg_name}"
+variable "sandbox_id" {
+  description = "Colony sandbox id"
+}
+
+resource "azurerm_resource_group" "test" {
+  name = "${var.rg_name}-${var.sandbox_id}"
+  location = "West Europe"
 }
 
 output "rg_id" {
