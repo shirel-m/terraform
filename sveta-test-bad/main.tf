166762950184b9910fd70210dabbb989d86e569c
svetaquali <svetlana.c@quali.com>
2019-12-03 19:12:49 +0200

    use env vars

--- sveta-test-bad/main.tf
+++ sveta-test-bad/main.tf
@@ -1,9 +1,8 @@
 provider "azurerm" {
   version = "=1.36.0"
 
-  use_msi = true
+  # use_msi = true
   # subscription_id = "${var.subscription_id}"
-  skip_provider_registration = true
 }
 
 variable "subscription_id" {
@@ -18,12 +17,20 @@ variable "sandbox_id" {
   description = "Colony sandbox id"
 }
 
-resource "azurerm_resource_group" "test" {
-  name = "${var.rg_name}-${var.sandbox_id}"
-  location = "West Europe"
+# resource "azurerm_resource_group" "test" {
+#   name = "${var.rg_name}-${var.sandbox_id}"
+#   location = "West Europe"
+# }
+
+# output "rg_id" {
+#   value = "${azurerm_resource_group.test.id}"
+# }
+
+data "azurerm_resource_group" "test" {
+  name = "${var.rg_name}"
 }
 
 output "rg_id" {
-  value = "${azurerm_resource_group.test.id}"
+  value = "${data.azurerm_resource_group.test.id}"
 }
 
