d06f40c08e1391eab1d252a1cc9888418fd1b255
svetaquali <svetlana.c@quali.com>
2019-12-03 15:22:59 +0200

    sveta-test-bad

---
+++ sveta-test-bad/main.tf
@@ -0,0 +1,28 @@
+provider "azurerm" {
+  version = "=1.36.0"
+
+  use_msi = true
+  subscription_id = "${var.subscription_id}"
+}
+
+variable "subscription_id" {
+  description = "The Subscription ID which should be used"
+}
+
+variable "rg_name" {
+  description = "resource group name"
+}
+
+variable "sandbox_id" {
+  description = "Colony sandbox id"
+}
+
+resource "azurerm_resource_group" "test" {
+  name = "${var.rg_name}-${var.sandbox_id}"
+  location = "West Europe"
+}
+
+output "rg_id" {
+  value = "${data.azurerm_resource_group.test.id}"
+}
+
