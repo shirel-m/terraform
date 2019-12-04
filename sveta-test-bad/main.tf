eda662220cac4b7824603565ec63d797bd9d2bbb
svetaquali <svetlana.c@quali.com>
2019-12-04 10:26:10 +0200

    minimal provider config

--- sveta-test-bad/main.tf
+++ sveta-test-bad/main.tf
@@ -1,12 +1,5 @@
 provider "azurerm" {
   version = "=1.36.0"
-
-  # use_msi = true
-  # subscription_id = "${var.subscription_id}"
-}
-
-variable "subscription_id" {
-  description = "The Subscription ID which should be used"
 }
 
 variable "rg_name" {
@@ -17,20 +10,20 @@ variable "sandbox_id" {
   description = "Colony sandbox id"
 }
 
-# resource "azurerm_resource_group" "test" {
-#   name = "${var.rg_name}-${var.sandbox_id}"
-#   location = "West Europe"
-# }
-
-# output "rg_id" {
-#   value = "${azurerm_resource_group.test.id}"
-# }
-
-data "azurerm_resource_group" "test" {
-  name = "${var.rg_name}"
+resource "azurerm_resource_group" "test" {
+  name = "${var.rg_name}-${var.sandbox_id}"
+  location = "West Europe"
 }
 
 output "rg_id" {
-  value = "${data.azurerm_resource_group.test.id}"
+  value = "${azurerm_resource_group.test.id}"
 }
+
+# data "azurerm_resource_group" "test" {
+#   name = "${var.rg_name}"
+# }
+
+# output "rg_id" {
+#   value = "${data.azurerm_resource_group.test.id}"
+# }
 
