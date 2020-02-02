26a70e31314b9265214fcd2046ced04dd4b4c228
svetaquali <svetlana.c@quali.com>
2020-02-02 12:58:11 +0200

    azure-storage-blob-writer

---
+++ azure-storage-blob-writer/main.tf
@@ -0,0 +1,12 @@
+provider "azurerm" {
+  version = "=1.35.0"   # the earliest version we support for msi
+}
+
+resource "azurerm_storage_blob" "my_blob" {
+  name                   = "${var.blob_name}"
+  resource_group_name    = "${var.rg_name}"
+  storage_account_name   = "${var.storage_account_name}"
+  storage_container_name = "${var.storage_container_name}"
+  type                   = "Block"
+  source_content         = "${var.content}"
+}
---
+++ azure-storage-blob-writer/variables.tf
@@ -0,0 +1,19 @@
+variable "rg_name" {
+  description = "resource group where the storage account resides"
+}
+
+variable "storage_account_name" {
+  description = "storage account name"
+}
+
+variable "storage_container_name" {
+  description = "storage container name"
+}
+
+variable "blob_name" {
+  description = "storage blob name"
+}
+
+variable "content" {
+  description = "the content to be written"
+}
