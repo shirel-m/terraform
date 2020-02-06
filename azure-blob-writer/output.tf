43b3394b21ce2b28d27298f7eb5b7ee06379edae
svetaquali <svetlana.c@quali.com>
2020-02-06 15:59:24 +0200

    azure-blob-writer

---
+++ azure-blob-writer/output.tf
@@ -0,0 +1,8 @@
+output "sandbox" {
+  value = "${var.SANDBOX_ID}"
+}
+
+output "container" {
+  value = "${var.storage_container_name}"
+}
+
---
+++ azure-blob-writer/variables.tf
@@ -0,0 +1,39 @@
+variable "storage_account_name" {
+}
+
+variable "storage_container_name" {
+}
+
+variable "USER_CONTENT" {
+}
+
+variable "SERVER_NAME" {
+}
+
+variable "SERVER_VERSION" {
+}
+
+variable "OUTPUT_PUBLISHER_STR" {
+}
+
+variable "VIRTUAL_NETWORK_ID" {
+}
+
+variable "PUBLIC_ADDRESS" {
+}
+
+variable "PUBLIC_ADDRESS_OLD" {
+}
+
+variable "SANDBOX_ID" {
+}
+
+variable "SANDBOX_ID_OLD" {
+}
+
+variable "OUTPUTS" {
+}
+
+variable "ALL_IN_ONE" {
+}
+
