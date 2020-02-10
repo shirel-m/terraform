3aa58dfa09a079d91679d69f52489c94ec16cdb5
svetaquali <svetlana.c@quali.com>
2020-02-10 18:56:01 +0200

    another go

--- azure-storage-blob-writer/presign.sh
+++ azure-storage-blob-writer/presign.sh
@@ -6,24 +6,26 @@ BlobName=$3
 BlobUrl=$4
 
 # install az
-apt-get update
-apt-get install ca-certificates curl apt-transport-https lsb-release gnupg
+# apt-get update
+# apt-get install ca-certificates curl apt-transport-https lsb-release gnupg
 
-curl -sL https://packages.microsoft.com/keys/microsoft.asc | 
-    gpg --dearmor | 
-    tee /etc/apt/trusted.gpg.d/microsoft.asc.gpg > /dev/null
+# curl -sL https://packages.microsoft.com/keys/microsoft.asc | 
+#     gpg --dearmor | 
+#     tee /etc/apt/trusted.gpg.d/microsoft.asc.gpg > /dev/null
 
-AZ_REPO=$(lsb_release -cs)
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | 
-    tee /etc/apt/sources.list.d/azure-cli.list
+# AZ_REPO=$(lsb_release -cs)
+# echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | 
+#     tee /etc/apt/sources.list.d/azure-cli.list
 
-apt-get update
-apt-get install azure-cli
+# apt-get update
+# apt-get install azure-cli
+
+curl -sL https://aka.ms/InstallAzureCLIDeb | bash
 # end install az
 
 az login --identity -u $ARM_CLIENT_ID
 if [[ $? -ne 0 ]]; then
-    &2 echo "az login failed"
+    echo "az login failed" >&2
 fi
 
 end=`date -u -d "30 minutes" '+%Y-%m-%dT%H:%MZ'`
