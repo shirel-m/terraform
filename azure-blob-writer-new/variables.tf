d4e204777f9f8ee2c7adf855a10ca58168d08cf5
svetaquali <svetlana.c@quali.com>
2020-02-27 15:04:05 +0200

    azure-blob-writer-new

---
+++ azure-blob-writer-new/variables.tf
@@ -0,0 +1,46 @@
+variable "storage_account_resource_group" {
+    default = ""
+}
+
+variable "storage_account_name" {
+}
+
+variable "storage_container_name" {
+}
+
+variable "blob_name" {
+}
+
+variable "USER_CONTENT" {
+}
+
+variable "SERVER_NAME" {
+}
+
+variable "SERVER_VERSION" {
+}
+
+variable "OUTPUT_PUBLISHER_STR" {
+}
+
+variable "VIRTUAL_NETWORK_ID" {
+}
+
+variable "PUBLIC_ADDRESS" {
+}
+
+variable "PUBLIC_ADDRESS_OLD" {
+}
+
+variable "SANDBOX_ID" {
+}
+
+variable "SANDBOX_ID_OLD" {
+}
+
+variable "OUTPUTS" {
+}
+
+variable "ALL_IN_ONE" {
+}
+
