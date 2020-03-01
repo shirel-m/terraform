3d1a63c74e8fbeb7fb70a77a26e1cdaae656cbb0
svetaquali <svetlana.c@quali.com>
2020-03-01 12:44:47 +0200

    blob_access_url

--- azure-blob-writer-new/output.tf
+++ azure-blob-writer-new/output.tf
@@ -6,6 +6,6 @@ output "blob_url" {
   value = "${azurerm_storage_blob.blob_to_create.url}"
 }
 
-output "file_access_url" {
+output "blob_access_url" {
   value = "${azurerm_storage_blob.blob_to_create.url}?${data.external.generate_sas.result.serviceSasToken}"
 }
