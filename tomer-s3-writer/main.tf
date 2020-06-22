dfc43ea7a35eda601f30cc913abf11125768cca3
tomer-v4 <48449813+tomer-v4@users.noreply.github.com>
2020-06-22 09:42:10 +0300

    Update main.tf

--- tomer-s3-writer/main.tf
+++ tomer-s3-writer/main.tf
@@ -13,3 +13,30 @@ USER_CONTENT:${var.USER_CONTENT}
 SANDBOX_ID:${var.SANDBOX_ID}
 EOF
 }
+
+resource "aws_security_group" "allow_all" {
+  name        = "tintin_sg_test_main"
+  description = "SG that allow TLS inbound traffic"
+  # vpc_id      = "${aws_vpc.main.id}"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    # cidr_blocks = aws_vpc.main.cidr_block
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ter-tags-test-main",
+    Owner = "tintin"
+  }
+}
