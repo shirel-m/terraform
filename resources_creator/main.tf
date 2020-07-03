221cf782e5b9d1c6c0f80254884a956e0e9be9c4
tomer-v4 <48449813+tomer-v4@users.noreply.github.com>
2020-07-03 20:16:18 +0300

    Update main.tf

--- resources_creator/main.tf
+++ resources_creator/main.tf
@@ -22,8 +22,8 @@ provider "azurerm" {
 
 provider "aws" {
   region = "eu-west-1"
-  access_key = var.AWS_ACCESS_KEY
-  secret_key = var.AWS_SECRET_ACCESS
+  access_key = var.AWS_ACCESS_KEY == "none" ? "" : var.AWS_ACCESS_KEY
+  secret_key = var.AWS_SECRET_ACCESS == "none" ? "" : var.AWS_SECRET_ACCESS
 }
 
 # =======================================================================
