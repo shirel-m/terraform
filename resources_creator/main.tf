58b1dec3a164242595c284772c6827bc79f53277
tomer-v4 <48449813+tomer-v4@users.noreply.github.com>
2020-07-05 17:39:33 +0300

    Update main.tf

--- resources_creator/main.tf
+++ resources_creator/main.tf
@@ -16,40 +16,40 @@ variable "SANDBOX_ID" {
 
 # =======================================================================
 
-# provider "azurerm" {
-#   version = "=1.35.0"   # the earliest version we support for msi
-# }
-
-provider "aws" {
-  region = "eu-west-1"
-  access_key = var.AWS_ACCESS_KEY == "none" ? "" : var.AWS_ACCESS_KEY
-  secret_key = var.AWS_SECRET_ACCESS == "none" ? "" : var.AWS_SECRET_ACCESS
+provider "azurerm" {
+  version = "=1.35.0"   # the earliest version we support for msi
 }
+
+# provider "aws" {
+#   region = "eu-west-1"
+#   access_key = var.AWS_ACCESS_KEY == "none" ? "" : var.AWS_ACCESS_KEY
+#   secret_key = var.AWS_SECRET_ACCESS == "none" ? "" : var.AWS_SECRET_ACCESS
+# }
 
 # =======================================================================
 
 # create a resource group on azure
 
-# resource "azurerm_resource_group" "rg" {
-#   name = "${var.RG_NAME}"
-#   location = "West Europe"
-# }
+resource "azurerm_resource_group" "rg" {
+  name = "${var.RG_NAME}"
+  location = "West Europe"
+}
 
 # create a file in bucket it aws
 
-resource "aws_s3_bucket_object" "object" {
-  bucket  = "${var.BUCKET_NAME}"
-  key     = "${var.SANDBOX_ID}.json"
-  acl     = "bucket-owner-full-control"
-  content = <<EOF
-SANDBOX_ID:${var.SANDBOX_ID}
-EOF
-}
+# resource "aws_s3_bucket_object" "object" {
+#   bucket  = "${var.BUCKET_NAME}"
+#   key     = "${var.SANDBOX_ID}.json"
+#   acl     = "bucket-owner-full-control"
+#   content = <<EOF
+# SANDBOX_ID:${var.SANDBOX_ID}
+# EOF
+# }
 
 
 # =======================================================================
 
 output "RG_ID" {
-  #value = "${azurerm_resource_group.rg.id}"
-  value = "koko"
+  value = "${azurerm_resource_group.rg.id}"
+  #value = "koko"
 }
