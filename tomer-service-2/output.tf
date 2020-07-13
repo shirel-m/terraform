047ee43b419fad6e8227de1ee86f508ba0d8cccf
tomer-v4 <tomer.v4@gmail.com>
2020-07-13 17:06:12 +0300

    added tomer-service-2

---
+++ tomer-service-2/output.tf
@@ -0,0 +1,7 @@
+output "output1" {
+  value = "${var.serv_input_1}"
+}
+
+output "output2" {
+  value = "${var.serv_input_2}"
+}
---
+++ tomer-service-2/variables.tf
@@ -0,0 +1,2 @@
+variable "serv_input_1" {}
+variable "serv_input_2" {}
