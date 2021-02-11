cbe915821630b79cb2495e864922115f70ed842d
yakirlevi <yakir.l@quali.com>
2021-02-11 14:21:58 +0200

    added outputs-publisher2 for testing

---
+++ outputs-publisher2/output.tf
@@ -0,0 +1,16 @@
+output "str" {
+  value = "${var.str}"
+}
+
+output "list" {
+  value = "${var.list}"
+}
+
+output "map" {
+  value = "${var.map}"
+}
+
+output "obj" {
+   value = "${var.obj}"
+}
+
