63247e6d3dcb1c6ecf73261fda1ff51922e76454
tomer-v4 <48449813+tomer-v4@users.noreply.github.com>
2021-02-17 13:03:33 +0200

    Update main.tf

--- tomer-s3-writer/main.tf
+++ tomer-s3-writer/main.tf
@@ -13,23 +13,23 @@ locals {
   }
 }
 
-# resource "aws_s3_bucket_object" "object" {
-#   bucket  = "${var.BUCKET_NAME}"
-#   key     = "${var.SANDBOX_ID}.json"
-#   acl     = "bucket-owner-full-control"
-#   content = <<EOF
-# USER_CONTENT:${var.USER_CONTENT}
-# SANDBOX_ID:${var.SANDBOX_ID}
-# EOF
-# }
-
-resource "aws_security_group" "zoro6" {
-  name        = "tintin_sg_test6"
-  tags = merge(
-    {
-      "age" = format("%s", "10")
-    },
-    local.common_tags,
-    local.extra_tags,
-  )
+resource "aws_s3_bucket_object" "object" {
+  bucket  = "${var.BUCKET_NAME}"
+  key     = "${var.SANDBOX_ID}.json"
+  acl     = "bucket-owner-full-control"
+  content = <<EOF
+USER_CONTENT:${var.USER_CONTENT}
+SANDBOX_ID:${var.SANDBOX_ID}
+EOF
 }
+
+# resource "aws_security_group" "zoro6" {
+#   name        = "tintin_sg_test6"
+#   tags = merge(
+#     {
+#       "age" = format("%s", "10")
+#     },
+#     local.common_tags,
+#     local.extra_tags,
+#   )
+# }
