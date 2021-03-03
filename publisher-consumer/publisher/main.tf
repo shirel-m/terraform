67c952f189734b5d678af2675297352edcd4a822
Stan Belkin <stan.b@quali.com>
2021-03-03 13:09:35 +0200

    remove Stan tags

--- publisher-consumer/publisher/main.tf
+++ publisher-consumer/publisher/main.tf
@@ -4,21 +4,11 @@ provider "aws" {
 
 resource "aws_vpc" "vpc" {
   cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Owner = "Stan"
-    Name = "stan-test-vpc"
-  }
 }
 
 resource "aws_subnet" "subnet" {
   vpc_id = aws_vpc.vpc.id
   cidr_block = "10.0.1.0/24"
-
-  tags = {
-    Owner = "Stan"
-    Name = "stan-test-subnet"
-  }
 }
 
 output "subnet_arn" {
