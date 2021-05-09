0331eeec282e0905c50ce8d87be91b0cbb9b9ec3
shirel-m <77104842+shirel-m@users.noreply.github.com>
2021-05-09 18:16:20 +0300

    outputs-publisher revert changes

---
+++ outputs-publisher/output.tf
@@ -0,0 +1,19 @@
+output "str" {
+  value = "${var.str}"
+}
+
+output "list" {
+  value = "${var.list}"
+}
+
+output "map" {
+  value = "${var.map}"
+}
+
+output "obj" {
+   value = "${var.obj}"
+}
+
+output "special_chars_str" {
+  value = "some::text:with-chars-4&more/chars_123^value"
+}
