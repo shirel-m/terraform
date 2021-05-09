eb892a09362e2b70c678ecb17674d992e3ed0195
shirel-m <77104842+shirel-m@users.noreply.github.com>
2021-05-09 18:00:02 +0300

    added output-publisher-tfv12

--- outputs-publisher-tfv12/output.tf
+++ outputs-publisher-tfv12/output.tf
@@ -1,23 +1,3 @@
-output "str" {
-  value = "${var.str}"
-}
-
-output "list" {
-  value = "${var.list}"
-}
-
-output "map" {
-  value = "${var.map}"
-}
-
-output "obj" {
-   value = "${var.obj}"
-}
-
-output "special_chars_str" {
-  value = "some::text:with-chars-4&more/chars_123^value"
-}
-
 output "num" {
   value = "${var.num}"
 }
