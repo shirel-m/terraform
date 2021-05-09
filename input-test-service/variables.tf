cefb8fafe32e527ccc9d9c56a7790d6453661322
shirel-m <77104842+shirel-m@users.noreply.github.com>
2021-05-09 13:00:18 +0300

    added terraform services for testing

---
+++ input-test-service/variables.tf
@@ -0,0 +1,12 @@
+
+variable "int_input" {
+    type = number
+}
+
+variable "float_input" {
+    type = number
+}
+
+variable "bool_input" {
+    type = bool
+}
--- output-test-service/outputs.tf
+++ output-test-service/outputs.tf
@@ -9,11 +9,3 @@ output "bool_output" {
 output "float_output" {
   value = tonumber("0.01")
 }
-
-output "date_output" {
-  value = formatdate("MMM DD, YYYY", "2018-01-02T23:12:01Z")
-}
-
-output "string_output" {
-  value = "'\"\\/"
-}
