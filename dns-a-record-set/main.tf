f94b5d7930ce252f58c0ad5cb46ffa296ca3bc78
shirel-m <77104842+shirel-m@users.noreply.github.com>
2021-06-28 17:44:18 +0300

    fixed dns-a-record-set

---
+++ dns-a-record-set/main.tf
@@ -0,0 +1,37 @@
+variable "DOMAIN" {
+  type = string
+}
+
+variable "HOST" {
+  type = string
+}
+
+variable "SANDBOX_PUBLIC_ADDRESS" {
+  type = string
+}
+
+provider "aws" {
+  region = "eu-west-1"
+  assume_role {
+    role_arn     = "arn:aws:iam::513341655526:role/colony/7ee79e8a-785b-ef52-11a1-17ef95ee3344/colony-power-user-role-7ee79e8a-785b-ef52-11a1-17ef95ee3344"
+    session_name = "DNS_SET_A_RECORD_SESSION"
+  }
+}
+
+data "aws_elb_hosted_zone_id" "main" {}
+
+data "aws_route53_zone" "domain" {
+  name         = var.DOMAIN
+  private_zone = false
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = data.aws_route53_zone.domain.zone_id
+  name    = var.HOST
+  type    = "A"
+  alias {
+    name                   = var.SANDBOX_PUBLIC_ADDRESS
+    zone_id                = data.aws_elb_hosted_zone_id.main.id
+    evaluate_target_health = true
+  }
+}
