24bd66f9179607112e58873eb0e0d6e75e5a2601
shirel-m <77104842+shirel-m@users.noreply.github.com>
2021-11-23 12:08:33 +0200

    added terraform module for debugging

---
+++ sleep-short-term/outputs.tf
@@ -0,0 +1,15 @@
+output "hostname" {
+  value = "amir"
+}
+
+output "output1" {
+  value = "test"
+}
+
+output "output2" {
+  value = "test"
+}
+
+output "output3" {
+  value = "test"
+}
---
+++ sleep-short-term/vars.tf
@@ -0,0 +1,7 @@
+variable DURATION {}
+variable "INPUT1" {}
+variable "INPUT2" {}
+variable "INPUT3" {}
+variable "INPUT4" {}
+variable "INPUT5" {}
+variable "INPUT6" {}
