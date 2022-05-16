22fac0878b7096a818517de77d29ee48b3c17a85
shirel-m <77104842+shirel-m@users.noreply.github.com>
2022-05-16 15:43:46 +0300

    fix outputs publisher

--- outputs-publisher/variables.tf
+++ outputs-publisher/variables.tf
@@ -18,17 +18,22 @@ variable "map" {
 }
 
 variable "obj" {
-  type = map
+  type = object({
+    age = number
+    name = string
+    user = list(map)
+    data = string
+  })
   default = {
-    "age" = 52,
-    "name" = "John",
-    "user" = [
+    age = 52,
+    name = "John",
+    user = [
       {
         "user-a" = "a-topic",
         "user-b" = "c-topic",
         "user-c" = "c-topic"
       }
     ],
-    "data" = "/!@#$@#%#$^!!~///!~\"!@#$!#~~~//##\\$\\#$%^&*(&*()sa\\df\nAFSDRFQW1'2''3'''4''''SDFSDF\\sdfrt\\\tt{}|||'''''df'"
+    data = "/!@#$@#%#$^!!~///!~\"!@#$!#~~~//##\\$\\#$%^&*(&*()sa\\df\nAFSDRFQW1'2''3'''4''''SDFSDF\\sdfrt\\\tt{}|||'''''df'"
   }           
 }
